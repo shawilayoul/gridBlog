4e0db34e25c1221210c541aa5f845272463c2ed2
shawil ayoul <aocholayoul9@gmail.com>
2024-04-10 23:52:24 +0200

    working on design

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const articleContainer = document.querySelector(".article-container")
+const articleContainer = document.querySelector(".article-container");
 
-
-const getarticle = async()=>{
+const getarticle = async () => {
   let response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  let articles = await response.json()
-   articles.map(({body,title})=>{
+  //let response = await fetch("https://fakestoreapi.com/products");
+  let articles = await response.json();
+  articles.map(({ title, body }) => {
     const tamplate = `<article class="shadow rounded p-2 m-3">
     <h2 class="font-bold">${title}</h2>
     <p class="font-semibold">Author</p>
-    <p>10/10/2020</p>
     <p>
      ${body}
     </p>
     <a href="article1.html" class="text-blue-700">read more</a>
-  </article>`
-  articleContainer.innerHTML += tamplate;
-   })
+  </article>`;
+    articleContainer.innerHTML += tamplate;
+  });
+};
 
-}
-
-getarticle()
+getarticle();
